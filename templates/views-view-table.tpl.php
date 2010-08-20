bf1feddb4178dd3903dc523c40d7bd4246f1ca85
Young Hahn <young@developmentseed.org>
2010-08-20 16:05:38 -0400

    Update views table template.

--- templates/views-view-table.tpl.php
+++ templates/views-view-table.tpl.php
@@ -27,16 +27,14 @@ if (empty($test)) {
     </thead>
   <?php endif; ?>
   <tbody>
-    <?php $count = 0; ?>
-    <?php foreach ($rows as $class => $row): ?>
-      <tr class="<?php if (!is_numeric($class)) print $class ?> <?php print is_numeric($count) && ($count % 2 == 0) ? 'even' : 'odd';?>">
+    <?php foreach ($rows as $count => $row): ?>
+      <tr class="<?php print implode(' ', $row_classes[$count]); ?>">
         <?php foreach ($row as $field => $content): ?>
           <td class="views-field <?php print $fields[$field]; ?>">
             <?php print $content; ?>
           </td>
         <?php endforeach; ?>
       </tr>
-      <?php $count++; ?>
     <?php endforeach; ?>
   </tbody>
 </table>
